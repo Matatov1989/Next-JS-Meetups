a770c3b9e51b95cd7ea709a488d202c65837c62e
Yury <matatov1989@gmail.com>
2023-11-14 16:36:23 +0200

    introducing api routes

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -5,4 +5,31 @@ function MeetupDetails() {
   return <MeetupDetail />;
 }
 
+export function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    // props: {
+    //   meetupData: ,
+    // },
+    // revalidate: 1,
+  };
+}
+
 export default MeetupDetails;
