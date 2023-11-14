2ebe96e6283cfecdfc1103e341a431edce1f1f33
Yury <matatov1989@gmail.com>
2023-11-14 19:49:34 +0200

    implement mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,12 @@
+import { MongoClient } from "mongodb";
+
 function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
     const { title, image, address, description } = data;
 
-    
+    MongoClient.connect();
   }
 }
 
