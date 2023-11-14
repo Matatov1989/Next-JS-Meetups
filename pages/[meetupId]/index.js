3e38923d05fcc3cbb3eb5d36f85e3d3ead0290ae
Yury <matatov1989@gmail.com>
2023-11-14 14:58:39 +0200

    adding custom components & css modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,8 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+import { Fragment } from "react";
+
+function MeetupDetails() {
+  return <MeetupDetail />;
+}
+
+export default MeetupDetails;
